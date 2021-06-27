f13ac131b61ea113820df1f70fa64694d0682256
chandrikasingh <chandrikasingh8@gmail.com>
2021-06-28 00:31:56 +0530

    Delete app.js

--- .github/workflows/app.js
+++
@@ -1,9 +0,0 @@
-const hero = document.getElementById("hero");
-const slider = document.getElementById("slider");
-const logo = document.getElementById("logo");
-const hamburger = document.getElementById("hamburger");
-const headline = document.getElementById("headline");
-
-// const t1 = new TimelineMax();
-// t1.fromTo(hero,1, {height: "0"}, {height: "80%", ease: Power.easeInOut});
-
